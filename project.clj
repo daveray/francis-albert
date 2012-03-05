d7c06e4d333fdd5c1e403b7ab624268ce51646ba
Dave Ray <daveray@gmail.com>
2012-03-04 21:44:07 -0500

    Update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject francis-albert "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject francis-albert "0.0.0-SNAPSHOT"
+  :description "Various ear training stuff implemented with Overtone"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.5.0"]
-                 [seesaw "1.2.1"]]
+                 [overtone "0.6.0"]
+                 [seesaw "1.3.0"]]
   :main francis-albert.core)
